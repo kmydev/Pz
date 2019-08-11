7bf1e84911c119c0da8f25a72543d3fb1ecf9001
KOMIYA Motofumi <komiya.dev0@gmail.com>
2019-08-11 15:59:51 +0900

    Create d002.cs

---
+++ d002.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Paiza{
+    public static void Main(){
+        string line = Console.ReadLine().Trim();
+        string[] arr = line.Split(' ');
+        int a = int.Parse(arr[0]);
+        int b = int.Parse(arr[1]);
+        string ans = (a == b) ? "eq" : (a > b) ? a.ToString() : b.ToString();
+        System.Console.WriteLine(ans);
+    }
+}
