0741aa73f713d4e18149c63e8e26a73bd3823434
KOMIYA Motofumi <komiya.dev0@gmail.com>
2019-08-11 16:03:52 +0900

    Create d003.cs

---
+++ d003.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Text;
+
+public class Paiza{
+    public static void Main(){
+        string line = Console.ReadLine().Trim();
+        int n = int.Parse(line);
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 1; i <= 9; i++)
+        {
+            sb.Append(i * n);
+            sb.Append(" ");
+        }
+
+        System.Console.WriteLine(sb.ToString().Trim());
+    }
+}
