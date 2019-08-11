184ad5280fdfac928ebf3fbdda90c11099a3b208
KOMIYA Motofumi <komiya.dev0@gmail.com>
2019-08-11 16:19:28 +0900

    Create d006.cs

---
+++ d006.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Text;
+
+public class Paiza{
+    public static void Main(){
+        string line = Console.ReadLine().Trim();
+        string[] arr = line.Split(' ');
+        int n = int.Parse(arr[0]);
+        string s = arr[1];
+        int multi = 1;
+
+        if (s == "km")
+        {
+            multi *= 1000000;
+        }
+        else if (s == "m")
+        {
+            multi *= 1000;
+        }
+        else if (s == "cm")
+        {
+            multi *= 10;
+        }
+
+        Console.WriteLine((n * multi).ToString());
+    }
+}
