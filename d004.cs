46bc0afd7bbdc5e1b8559920051d17fa3bc5129d
KOMIYA Motofumi <komiya.dev0@gmail.com>
2019-08-11 16:09:00 +0900

    Create d004.cs

---
+++ d004.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Text;
+
+public class Paiza{
+    public static void Main(){
+        string line = Console.ReadLine().Trim();
+        int n = int.Parse(line);
+
+        StringBuilder sb = new StringBuilder();
+        sb.Append("Hello ");
+
+        for (int i = 0; i < n; i++)
+        {
+            sb.Append(Console.ReadLine().Trim());
+            if (i < n - 1) sb.Append(",");
+        }
+        sb.Append(".");
+
+        System.Console.WriteLine(sb.ToString().Trim());
+    }
+}
