0f094ed24f235f3e447c7b4b7ee403838a228fea
kmydev <komiya.dev0@gmail.com>
2019-08-12 19:22:14 +0900

    Create d010.cs

---
+++ d010.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Text;
+
+public class Pz{
+    public static void Main(){
+        string s = Console.ReadLine().Trim();
+        string t = Console.ReadLine().Trim();
+
+        Console.WriteLine("{0}@{1}", s, t);
+    }
+}
