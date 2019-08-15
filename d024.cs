f9190cbd613c8ec2cea5ec6b3bb19245e3b07c73
kmydev <komiya.dev0@gmail.com>
2019-08-16 06:30:04 +0900

    Create d024.cs

---
+++ d024.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Pz{
+    public static void Main(){
+        int a = int.Parse(Console.ReadLine().Trim());
+        int b = int.Parse(Console.ReadLine().Trim());
+
+        Console.WriteLine(180 - a - b);
+    }
+}
