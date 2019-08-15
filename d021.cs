463f261a40834c246367af56bcc5ba0f0bfeaa3d
kmydev <komiya.dev0@gmail.com>
2019-08-16 06:19:06 +0900

    Create d021.cs

---
+++ d021.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Pz{
+    public static void Main(){
+        string s = Console.ReadLine().Trim();
+        string t = Console.ReadLine().Trim();
+
+        Console.WriteLine((s == t) ? "Yes" : "No");
+    }
+}
