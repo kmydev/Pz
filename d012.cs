800c99b6151b906c6b313a88fdfb459126a4d728
kmydev <komiya.dev0@gmail.com>
2019-08-15 16:50:27 +0900

    Create d012.cs

---
+++ d012.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Pz{
+    public static void Main(){
+        string s = Console.ReadLine().Trim();
+        int n = Math.Abs(int.Parse(s));
+        Console.WriteLine(n);
+    }
+}
