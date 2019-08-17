c054116bcc73bde1d38b9e76389f59c92948dbc6
kmydev <komiya.dev0@gmail.com>
2019-08-17 14:12:54 +0900

    Create d033.cs

---
+++ d033.cs
@@ -0,0 +1,8 @@
+using System;
+
+public class Pz{
+    public static void Main(){
+        string[] arr = Console.ReadLine().Trim().Split(' ');
+        Console.WriteLine("{0}.{1}", arr[0][0], arr[1][0]);
+    }
+}
