491ec1c0fdca76eab3a69bce805f8810fedff4d1
kmydev <komiya.dev0@gmail.com>
2019-08-18 04:56:45 +0900

    Create d037.cs

---
+++ d037.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Pz{
+    public static void Main(){
+        int m = int.Parse(Console.ReadLine().Trim());
+        int n = int.Parse(Console.ReadLine().Trim());
+        Console.WriteLine((n + m - 1) / m);
+    }
+}
