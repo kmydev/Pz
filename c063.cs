5f5179502685644b995ea6d49d877f2557126509
kmydev <komiya.dev0@gmail.com>
2019-08-18 17:36:56 +0900

    Update c063.cs

--- c063.cs
+++ c063.cs
@@ -12,20 +12,25 @@ public class Pz{
             var arr = Console.ReadLine().Trim().Split(' ');
             int a = int.Parse(arr[0]);
             int b = int.Parse(arr[1]);
-            tmpchr[a + b - 1] = '1';
+            if (a + b < 30) tmpchr[a + b - 1] = '1';
             flowersCalendar[i] = new string(tmpchr);
         }
 
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine(flowersCalendar[i]);
+        }
+        
         int max_day = 1;
         int max_flowers = 0;
-        for (int i = 0; i < 30; i++)
+        for (int i = 29; i >= 0; i--)
         {
             int cnt = 0;
             for (int j = 0; j < n; j++)
             {
                 if (flowersCalendar[j][i] == '1') cnt++;
             }
-            if (max_flowers < cnt)
+            if (max_flowers <= cnt)
             {
                 max_flowers = cnt;
                 max_day = i + 1;
