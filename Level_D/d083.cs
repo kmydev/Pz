69efffa3efe051feb2ba175ea42a16924db6c571
kmydev <komiya.dev0@gmail.com>
2019-10-02 12:56:01 +0900

    Create d083.cs

---
+++ Level_D/d083.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Pz{
+    public static void Main(){
+        var arr = Console.ReadLine().Trim().Split(' ');
+        int n = int.Parse(arr[0]);
+        int m = int.Parse(arr[1]);
+        Console.WriteLine((n + m < 16) ? "HIT" : "STAND");
+    }
+}
