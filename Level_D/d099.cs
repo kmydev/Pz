d2940e1777d9f9a2ee776f8058c5dbe1a8ace804
kmydev <komiya.dev0@gmail.com>
2019-10-19 12:18:51 +0900

    Create d099.cs

---
+++ Level_D/d099.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Pz{
+    public static void Main(){
+        string s = Console.ReadLine().Trim();
+        for (int i = 0; i < s.Length; i++)
+        {
+            Console.WriteLine(s[i]);
+        }
+    }
+}
