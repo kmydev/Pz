5ce82b5decb523eb060c32924dbe4443760cc27b
kmydev <komiya.dev0@gmail.com>
2019-10-19 14:04:48 +0900

    Create d105.cs

---
+++ Level_D/d105.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Pz{
+    public static void Main(){
+        string s1 = Console.ReadLine().Trim();
+        string s2 = Console.ReadLine().Trim();
+        Console.WriteLine((s1.Length == s2.Length) ? "Yes" : "No");
+    }
+}
