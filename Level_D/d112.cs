a698eeffd3256ef6a8310c412da540242a896f3c
kmydev <komiya.dev0@gmail.com>
2019-10-29 11:03:05 +0900

    Create d112.cs

---
+++ Level_D/d112.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Pz{
+    public static void Main(){
+        int n = int.Parse(Console.ReadLine().Trim());
+        int h = int.Parse(Console.ReadLine().Trim());
+        Console.WriteLine(n*h);
+    }
+}
