087da63e68029dacfeb938ce731a9779bf22d265
kmydev <komiya.dev0@gmail.com>
2019-11-25 21:15:05 +0900

    Create d140.cs

---
+++ Level_D/d140.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Pz{
+    public static void Main(){
+        int n = int.Parse(Console.ReadLine().Trim());
+        var arr = Console.ReadLine().Trim().Split(' ');
+        System.Console.WriteLine(arr[n]);
+    }
+}
