afa0e523df5aca801a7935dcd3486a5228a8459c
kmydev <komiya.dev0@gmail.com>
2020-05-21 19:46:57 +0900

    Create d154.cs

---
+++ Level_D/d154.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Pz{
+    public static void Main(){
+        int n = int.Parse(Console.ReadLine().Trim());
+        int m = int.Parse(Console.ReadLine().Trim());
+        Console.WriteLine(n*n-m);
+    }
+}
