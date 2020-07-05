1c4983ce86a22ab1f74502f41a0284963e9096b7
kmydev <komiya.dev0@gmail.com>
2020-07-05 16:29:37 +0900

    Create d156.cs

---
+++ Level_D/d156.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Pz{
+    public static void Main(){
+        int n = int.Parse(Console.ReadLine().Trim());
+        int m = int.Parse(Console.ReadLine().Trim());
+        Console.WriteLine(n*m);
+    }
+}
