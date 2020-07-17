575632e673d0d64452239dd8d285589754386bfa
kmydev <komiya.dev0@gmail.com>
2020-07-17 13:39:07 +0900

    Create d160.cs

---
+++ Level_D/d160.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+
+public class Pz{
+    public static void Main(){
+        int n = int.Parse(Console.ReadLine().Trim());
+        int m = int.Parse(Console.ReadLine().Trim());
+        Console.WriteLine(n/10*m);
+    }
+}
