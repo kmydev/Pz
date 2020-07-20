ae401c30a80b5488342500203cb8bdeb248dc062
kmydev <komiya.dev0@gmail.com>
2020-07-20 11:23:37 +0900

    Updated d017.cs

--- Level_D/d017.cs
+++ Level_D/d017.cs
@@ -1,17 +1,18 @@
 using System;
+using System.Text;
+using System.Collections.Generic;
 
 public class Pz{
     public static void Main(){
-        int max = 1;
-        int min = 99;
+        int max_n = 0;
+        int min_n = 100;
         for (int i = 0; i < 5; i++)
         {
             int n = int.Parse(Console.ReadLine().Trim());
-            max = (n > max) ? n : max;
-            min = (n < min) ? n : min;
+            if (n > max_n) max_n = n;
+            if (n < min_n) min_n = n;
         }
-
-        Console.WriteLine(max);
-        Console.WriteLine(min);
+        Console.WriteLine(max_n);
+        Console.WriteLine(min_n);
     }
 }
