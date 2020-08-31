7c2aa30803e6b9364ac46fb230260d1a6cb9c1d3
kmydev <komiya.dev0@gmail.com>
2020-08-31 20:22:34 +0900

    Create d163.cs

---
+++ Level_D/d163.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Pz{
+    public static void Main(){
+        var s = System.Console.ReadLine();
+        foreach (var ss in s)
+        {
+            Console.WriteLine(ss);   
+        }
+    }
+}
