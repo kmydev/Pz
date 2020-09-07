b88d47e867a03fe40724db5fb118e2ddf898c25d
kmydev <komiya.dev0@gmail.com>
2020-09-07 16:58:08 +0900

    Create d039.py

---
+++ Level_D/Python2/d039.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+if __name__=='__main__':
+    a = int(raw_input().strip())
+    b = int(raw_input().strip())
+    c = int(raw_input().strip())
+
+    if a == b and a == c:
+        print('YES')
+    else:
+        print('NO')
