690eb41287283bdf0974dfe0d2ed67d5dadd607b
kmydev <komiya.dev0@gmail.com>
2020-09-07 16:51:48 +0900

    Create d042.py

---
+++ Level_D/Python2/d042.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+if __name__=='__main__':
+    arr = raw_input().strip().split(' ')
+    n1 = int(arr[0])
+    n2 = int(arr[1])
+    arr = raw_input().strip().split(' ')
+    n3 = int(arr[0])
+    n4 = int(arr[1])
+    print(n1*n4-n2*n3)
