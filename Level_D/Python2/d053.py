7bca27b23f462ea9d673f6dc210c68b552a46ae4
kmydev <komiya.dev0@gmail.com>
2020-09-08 20:49:26 +0900

    Create d053.py

---
+++ Level_D/Python2/d053.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+if __name__=="__main__":
+    line = raw_input().strip()
+    if line == 'chocolate' or line == 'candy':
+        print('Thanks!')
+    else:
+        print('No!')
