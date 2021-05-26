82a11a8e41a1bdc66faf124bb5631901927c6d1c
kmydev <komiya.dev0@gmail.com>
2021-05-26 18:36:48 +0900

    Create d172.cs

---
+++ Level_D/d172.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Text;
+
+public class Pz{
+    public static void Main(){
+        int n = int.Parse(Console.ReadLine().Trim());
+        Console.WriteLine(n-1);
+    }
+}
