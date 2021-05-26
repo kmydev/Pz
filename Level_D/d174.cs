9c2bf5b7f47ab36370e69572aa782d2c402f1faa
kmydev <komiya.dev0@gmail.com>
2021-05-26 18:38:03 +0900

    Create d174.cs

---
+++ Level_D/d174.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Text;
+
+public class Pz{
+    public static void Main(){
+        int k = int.Parse(Console.ReadLine().Trim());
+        Console.WriteLine(k*1500);
+    }
+}
