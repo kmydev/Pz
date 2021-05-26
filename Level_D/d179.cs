dd79643cdbbfe26c62503467cd8753495ead08e3
kmydev <komiya.dev0@gmail.com>
2021-05-26 18:35:31 +0900

    Create d179.cs

---
+++ Level_D/d179.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Text;
+
+public class Pz{
+    public static void Main(){
+        int n = int.Parse(Console.ReadLine().Trim());
+        int m = int.Parse(Console.ReadLine().Trim());
+        Console.WriteLine(m % n);
+    }
+}
